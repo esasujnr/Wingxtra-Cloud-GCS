b6c9929069c057f9e5b5264c54be045e9cfab43e
esasujnr <esasujnr@gmail.com>
2026-02-15 13:00:27 +0000

    Update jsc_ctrl_layoutControl.jsx

--- src/components/jsc_ctrl_layoutControl.jsx
+++ src/components/jsc_ctrl_layoutControl.jsx
@@ -17,34 +17,10 @@ class ClssCtrlLayout extends React.Component {
     }
 
     componentWillUnmount() {
-        if (this.m_timer) clearInterval(this.m_timer);
-    }
-
-    fn_getDateLabel() {
-        return this.state.now.toLocaleDateString(undefined, {
-            year: 'numeric',
-            month: 'short',
-            day: '2-digit'
-        });
-    }
-
-    fn_getTimeLabel() {
-        return this.state.now.toLocaleTimeString(undefined, {
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: false
-        });
-    }
-
-    componentDidMount() {
-        this.m_timer = setInterval(() => {
-            this.setState({ now: new Date() });
-        }, 1000);
-    }
-
-    componentWillUnmount() {
-        if (this.m_timer) clearInterval(this.m_timer);
+        if (this.m_timer) {
+            clearInterval(this.m_timer);
+            this.m_timer = null;
+        }
     }
 
     fn_getDateLabel() {
@@ -66,13 +42,26 @@ class ClssCtrlLayout extends React.Component {
 
     render() {
         return (
-            <div id="main_btn_group" role="group" className="d-flex align-items-center justify-content-end">
-                <div id="vehicle_alert_space" className="css_vehicle_alert_space user-select-none" title="Vehicle alerts area">
+            <div
+                id="main_btn_group"
+                role="group"
+                className="d-flex align-items-center justify-content-end"
+            >
+                <div
+                    id="vehicle_alert_space"
+                    className="css_vehicle_alert_space user-select-none"
+                    title="Vehicle alerts area"
+                >
                     Vehicle Alerts: --
                 </div>
+
                 <div className="css_cockpit_datetime user-select-none ms-3">
-                    <span className="css_cockpit_time">{this.fn_getTimeLabel()}</span>
-                    <span className="css_cockpit_date">{this.fn_getDateLabel()}</span>
+                    <span className="css_cockpit_time">
+                        {this.fn_getTimeLabel()}
+                    </span>
+                    <span className="css_cockpit_date">
+                        {this.fn_getDateLabel()}
+                    </span>
                 </div>
             </div>
         );
